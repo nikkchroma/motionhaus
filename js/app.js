b574627850ebf9574d88a7646a50f66bf30d2da6
nikkchroma <nikhil.m.13.2004@gmail.com>
2026-07-12 00:21:34 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
-console.log('ok')
+gsap.from(".hero span",{
+
+opacity:0,
+
+y:50,
+
+duration:1
+
+});
+
+gsap.from(".hero h1",{
+
+opacity:0,
+
+y:80,
+
+delay:.3,
+
+duration:1
+
+});
+
+gsap.from(".hero p",{
+
+opacity:0,
+
+y:80,
+
+delay:.6,
+
+duration:1
+
+});
+
+gsap.from(".buttons",{
+
+opacity:0,
+
+y:80,
+
+delay:.9,
+
+duration:1
+
+});
